9cc84a3349ea9e0913e0ea20cf8ab1bf87a5cd8f
Khaalid245 <k.abdillahi@alustudent.com>
2025-05-18 18:43:14 +0200

    day-29

--- day-29/main.py
+++ day-29/main.py
@@ -1,13 +1,20 @@
+from Tools.scripts.objgraph import definitions
 from flask import Flask, render_template
 
 app = Flask("Website")
 
-@app.route("/home")
+@app.route("/")
 def home():
-    return  render_template("tutorial.html")
+    return  render_template("home.html")
 
-@app.route("/contact/")
-def contact():
-    return render_template("contact.html")
+@app.route("/api/v1/<station>/<date>")
+def contact(station,date):
+    temperature = 23
+    return {"station":station,
+            "date":date,
+            "temperature":temperature
+            }
 
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5002)
