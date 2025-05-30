11f3195729dd60bb3c5b481c84c51b41599f309c
Khaalid245 <k.abdillahi@alustudent.com>
2025-05-30 20:10:11 +0200

    I have done job application form

--- day-52/job_application/views.py
+++ day-52/job_application/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import ApplicationForm
 from  .models import Form
 from django.contrib import messages
+from  django.core.mail import EmailMessage
 
 def index (request):
     if request.method == "POST":
@@ -15,5 +16,10 @@ def index (request):
 
             Form.objects.create(first_name=first_name, last_name=last_name,
                                 email=email, date=date, occupation=occupation)
+
+            messages_body = f"A new jop application was submitted. thank you{first_name}."
+            email_message= EmailMessage("Form submission confirmation", messages_body, to=[email])
+            email_message.send()
+
             messages.success(request,"form submitted")
     return render(request,"index.html")
